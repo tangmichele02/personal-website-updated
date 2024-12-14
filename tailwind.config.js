a7a26df3a4bffea7cd7c848a072578b2c704c638
Michele Tang <tangmichele02@gmail.com>
2024-12-13 22:17:44 -0800

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',  // Add these paths to make sure Tailwind processes all your source files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
